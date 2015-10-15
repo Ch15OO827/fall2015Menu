afb5aaab24366e3a314b92e94daf7d9b9d0e720c
n02619263 <n02619263@hawkmail.newpaltz.edu>
2015-10-15 06:15:08 +0000

    THird

---
+++ php/workout/view.php
@@ -0,0 +1,90 @@
+<?php
+session_start();
+    $name = 'Erik Cabrera';
+    $message = "Welcome $name";
+    
+    $person = array( 'Name' => $name, 'Age' => 21, CalorieBurnGoal => 1000 );
+    
+    $workO = $_SESSION['workout'];
+    if(!$workO){
+      $_SESSION['workout'] = $workO = array(
+          );
+    }
+        
+    $total = 0;
+    foreach ($workO as $session) {
+        $total += $session['Calories'];
+    }
+    
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Workout Date</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  </head>
+  <body
+    <div class="container">
+            <h1>Workout Date</h1>
+            <h2><?=$message?></h2>
+            <div class="panel panel-success">
+                <div class="panel-heading">Your Data</div>
+                <div class="panel-body">
+                    <dl class="dl-horizontal">
+                        <dt>Name</dt>
+                        <dd><?=$person['Name']?></dd>
+                        <dt>Age</dt>
+                        <dd><?=$person['Age']?></dd>
+                        <dt>Goal</dt>
+                        <dd><?=$person['CalorieBurnGoal']?></dd>
+                        <dt>Burned Calories 2day</dt>
+                        <dd><?=$total?></dd>
+                    </dl>
+                </div>
+            </div>
+    </div>
+
+<div class="container">
+
+        <div class="page-header">
+          <h1>Workout Date <small>Record your daily sessions</small></h1>
+        </div>
+         
+        <form class="form-horizontal" action="" method="post" >
+          <div class='alert' style="display: none" id="myAlert">
+            <button type="button" class="close" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <h3></h3>
+          </div> 
+          <div class="form-group">
+            <label for="txtName" class="col-sm-2 control-label">Name</label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control" id="txtName" name="Name" placeholder="Workout Name" value="<?=$session['Name']?>">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-sm-2 control-label" for="txtCalories">Calories</label>
+            <div class="col-sm-10">
+                  <input type="number" class="form-control" id="txtCalories" name="Calories" placeholder="Calories burned in this session"  value="<?=$session['Calories']?>">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-sm-2 control-label" for="txtDate">When did you workout</label>
+            <div class="col-sm-10">
+                  <input type="text" class="form-control date" id="txtDate" name="Time" placeholder="Date"  value="<?=$session['Time']?>">
+            </div>
+          </div>
+        </form>
+    </div>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </body>
+</html>
