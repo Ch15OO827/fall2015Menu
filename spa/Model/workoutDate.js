5ecdbba01cc92d0aaa7395432a19ca02bd85b3bc
n02619263 <n02619263@hawkmail.newpaltz.edu>
2015-11-16 14:39:29 +0000

    added drinks, workoutDate.js

---
+++ spa/Model/workoutDate.js
@@ -0,0 +1,62 @@
+var mysql = require("mysql");
+
+module.exports =  {
+    blank: function(){ return {} },
+    get: function(id, ret){
+        var conn = GetConnection();
+        var sql = 'SELECT * FROM 2015Fall_WorkoutDate ';
+        if(id){
+          sql += " WHERE id = " + id;
+        }
+        conn.query(sql, function(err,rows){
+          ret(err,rows);
+          conn.end();
+        });        
+    },
+    delete: function(id, ret){
+        var conn = GetConnection();
+        conn.query("DELETE FROM 2015Fall_WorkoutDate WHERE id = " + id, function(err,rows){
+          ret(err);
+          conn.end();
+        });        
+    },
+    save: function(row, ret){
+        var sql;
+        var conn = GetConnection();
+        //  TODO Sanitize
+        if (row.id) {
+				  sql = " Update 22015Fall_WorkoutDate "
+							+ " Set Name=?, Date=?, Calories=? "
+						  + " WHERE id = ? ";
+			  }else{
+				  sql = "INSERT INTO 2015Fall_WorkoutDate "
+						  + " (Name, Date, created_at, Calories) "
+						  + "VALUES (?, ?, Now(), ?) ";				
+			  }
+
+        conn.query(sql, [row.Name, row.Date, row.id, row.Calories],function(err,data){
+          if(!err && !row.id){
+            row.id = data.insertId;
+          }
+          ret(err, row);
+          conn.end();
+        });        
+    },
+    validate: function(row){
+      var errors = {};
+      
+      if(!row.Name) errors.Name = "is required"; 
+      
+      return errors.length ? errors : false;
+    }
+};  
+
+function GetConnection(){
+        var conn = mysql.createConnection({
+          host: "localhost",
+          user: "n02619263",
+          password: "Killmaster08",
+          database: "c9"
+        });
+    return conn;
+}
